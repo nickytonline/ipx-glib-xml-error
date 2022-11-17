2efc2439375c41ac4beefc2fd19bc344942da161
Nick Taylor <nick@iamdeveloper.com>
2022-11-16 19:39:41 -0500

    chore: testing broken image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'owa-lb-test.thenational.academy',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
